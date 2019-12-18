185fda3900b137ac167d6f47543404c0749bff62
Zipofar Ago <admin@zipofar.ru>
2019-12-18 17:16:07 +0300

    Fix menu toggler

--- src/js/navbar-toggler.js
+++ src/js/navbar-toggler.js
@@ -7,6 +7,7 @@ export default (classNameToggler = 'navbar-toggler') => {
   elToggler.addEventListener('click', () => {
     const heightNavbar = elNavbar.clientHeight;
     const isShow = elNavbarWrapper.classList.contains('show');
+    console.log('btn ' + isShow)
     if (isShow) {
       elNavbarWrapper.classList.remove('show');
       elNavbarWrapper.style.height = '';
@@ -15,4 +16,17 @@ export default (classNameToggler = 'navbar-toggler') => {
       elNavbarWrapper.style.height = `calc(1rem + ${heightNavbar}px)`;
     }
   });
+
+  document.addEventListener('click', (e) => {
+    if (e.target === elToggler) {
+      return;
+    }
+
+    const isShow = elNavbarWrapper.classList.contains('show');
+    if (isShow) {
+      elNavbarWrapper.classList.remove('show');
+      elNavbarWrapper.style.height = '';
+    }
+  });
+
 };
