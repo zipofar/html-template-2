0aa492b3d0d479bfae00b0defbd9ddbb92424690
Zipofar <zipofar@gmail.com>
2019-12-25 23:20:45 +0300

    Add swipe to promo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style/style.scss';
 import setupMenuToggler from './js/navbar-toggler';
 import setupAdditionToCard from './js/popular-products';
+import setupPromo from './js/promo';
 
 setupMenuToggler();
 setupAdditionToCard();
+setupPromo();
---
+++ src/js/promo.js
@@ -0,0 +1,3 @@
+import Swiper from 'swiper';
+
+export default () => (new Swiper('.promo__item-right', {}));
