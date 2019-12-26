292b30029968d6ec5fd21905ae2b8a746c75bbe8
Zipofar Ago <admin@zipofar.ru>
2019-12-26 10:42:28 +0300

    Update promo carousel

--- src/js/promo.js
+++ src/js/promo.js
@@ -5,4 +5,11 @@ export default () => (new Swiper('.promo__item-right', {
     nextEl: '.promo__swiper-btn-next',
     prevEl: '.promo__swiper-btn-prev',
   },
+  pagination: {
+    el: '.carousel-controls',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + ' carousel-controls__btn"></span>';
+    },
+  },
 }));
